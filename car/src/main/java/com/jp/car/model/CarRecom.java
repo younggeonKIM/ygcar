4e92e238f07de3daf25935419520240c487538e3
Younggeon Kim <yongonkimu@gmail.com>
2023-04-13 13:57:57 +0900

    carInfo_carRecomDao

--- car/src/main/java/com/jp/car/model/CarRecom.java
+++ car/src/main/java/com/jp/car/model/CarRecom.java
@@ -2,7 +2,9 @@ package com.jp.car.model;
 
 public class CarRecom {
 	private String carName;
-	private int overLength; //overall length 
-	private int overWidth; //overall width
-	private int overHeight; //overall height
+	private int overLength; //overall length 全長(m)
+	private int overWidth; //overall width　全幅(m)
+	private int overHeight; //overall height　全高(m)
+	private int vehWeight; //vehicle weight 車両重量(kg)
+	
 }
